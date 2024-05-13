f25e5260d9be3fee13a88953cfeb34c433ddf396
Julius Tamale <tamalejulius5@gmail.com>
2024-05-13 05:50:33 +0300

    easybank challenge

--- script.js
+++ script.js
@@ -2,3 +2,40 @@
 const currentDate = new Date()
 
 document.querySelector('.year').innerText = currentDate.getFullYear()
+
+
+// toggle menu
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function () {
+    console.log('click hamburger');
+
+    if (header.classList.contains('open')) { // Close Hamburger Menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
+    }
+    else { // Open Hamburger Menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
+    }
+});
+
+
+// sticky navbar on scroll
+window.addEventListener('scroll', () => {
+    window.scrollY >= 100 ? header.classList.add('active') : header.classList.remove('active')
+})
